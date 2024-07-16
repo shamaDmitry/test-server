82a9a8e201f6f34cf0021a2b337565be538f4de1
shama <shama.dmitry@gmail.com>
2024-07-16 09:56:11 +0300

    add routes

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const { default: axios } = require("axios");
 const express = require("express");
+var cors = require("cors");
 
 const app = express();
 const PORT = 8000;
+app.use(cors());
 
-app.get("/", (req, res) => {
-  axios
-    .get("https://emma.maryland.gov/page.aspx/en/rfp/request_browse_public")
-    .then((response) => {
-      console.log("res", response);
-    });
+const TABLE_URL =
+  "https://emma.maryland.gov/page.aspx/en/rfp/request_browse_public";
 
-  res.send("Hello World");
+const ITEM_URL =
+  "https://emma.maryland.gov/page.aspx/en/bpm/process_manage_extranet";
+
+app.get("/api/scrape", (req, res) => {
+  axios.get(TABLE_URL).then((response) => {
+    return res.json(response.data);
+  });
+});
+
+app.get("/api/scrape/:id", (req, res) => {
+  const id = req.params.id;
+
+  axios.get(`${ITEM_URL}/${id}`).then((response) => {
+    return res.json(response.data);
+  });
 });
 
 app.get("/about", (req, res) => {
